fbb7391ba509bab83134c961b9046e15adb6f2a3
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-06-25 23:23:35 +0300

    Add files via upload

---
+++ Entity_Framework_Core/01.FirstTest/FirstSQLconnect/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = "Server=.;Database=SoftUni;Integrated Security=true;TrustServerCertificate=True";
+
+using SqlConnection connection = new SqlConnection(connectionString);
+
+connection.Open();
+
+SqlCommand command = new SqlCommand(
+    "SELECT COUNT(*) FROM Employees", connection);
+
+int eCnt = (int) command.ExecuteScalar();
+
+Console.WriteLine("Connected...");
+Console.WriteLine($"Employees are: {eCnt}");
