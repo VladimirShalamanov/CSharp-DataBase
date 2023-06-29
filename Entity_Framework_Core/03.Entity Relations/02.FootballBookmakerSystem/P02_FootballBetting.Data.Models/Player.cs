cb2441df9a5d9c178315fdc30b7d719a6777b772
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-06-29 23:07:50 +0300

    Add files via upload

---
+++ Entity_Framework_Core/03.Entity Relations/02.FootballBookmakerSystem/P02_FootballBetting.Data.Models/Player.cs
@@ -0,0 +1,41 @@
+﻿namespace P02_FootballBetting.Data.Models;
+
+using P02_FootballBetting.Data.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Player
+{
+    public Player()
+    {
+        this.PlayersStatistics = new HashSet<PlayerStatistic>();
+    }
+
+    // By default int and bool are NOT NULL + [Required]
+    [Key]
+    public int PlayerId { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstants.PlayerNameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    public int SquadNumber { get; set; }
+
+    // ? maybe player not in a team
+    // ? judge might not be happy
+
+    [ForeignKey(nameof(Team))]
+    public int TeamId { get; set; }
+    public virtual Team Team { get; set; } = null!;
+
+
+    [ForeignKey(nameof(Position))]
+    public int PositionId { get; set; }
+    public virtual Position Position { get; set; } = null!;
+
+
+    public bool IsInjured { get; set; }
+
+
+    public virtual ICollection<PlayerStatistic> PlayersStatistics { get; set; }
+}
---
+++ Entity_Framework_Core/03.Entity Relations/02.FootballBookmakerSystem/P02_FootballBetting.Data.Models/PlayerStatistic.cs
@@ -0,0 +1,24 @@
+﻿namespace P02_FootballBetting.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class PlayerStatistic
+{
+    [ForeignKey(nameof(Game))]    
+    public int GameId { get; set; }
+    public virtual Game Game { get; set; } = null!;
+
+
+    [ForeignKey(nameof(Player))]
+    public int PlayerId { get; set; }
+    public virtual Player Player { get; set; } = null!;
+
+
+    // judge might not be happy with [byte]
+    public byte ScoredGoals { get; set; }
+
+    public byte Assists { get; set; }
+
+    public byte MinutesPlayed { get; set; }
+}
