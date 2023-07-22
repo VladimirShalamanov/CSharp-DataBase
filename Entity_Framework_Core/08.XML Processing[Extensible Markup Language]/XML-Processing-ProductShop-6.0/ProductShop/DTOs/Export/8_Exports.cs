e53c36f5d19fbd1e293231a316d34bdbc4eb1573
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-07-22 23:21:56 +0300

    Add files via upload

---
+++ Entity_Framework_Core/08.XML Processing[Extensible Markup Language]/XML-Processing-ProductShop-6.0/ProductShop/DTOs/Export/8_Exports.cs
@@ -0,0 +1,51 @@
+﻿namespace ProductShop.DTOs.Export;
+
+using System.Xml.Serialization;
+
+public class _8_Exports
+{
+    [XmlType("Users")]
+    public class ExportUserCount
+    {
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlArray("users")]
+        public UserInfo[] Users { get; set; } = null!;
+    }
+
+    [XmlType("User")]
+    public class UserInfo
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; } = null!;
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; } = null!;
+
+        [XmlElement("age")]
+        public int? Age { get; set; }
+
+        public SoldProductsCount SouldProducts { get; set; } = null!;
+    }
+
+    [XmlType("SoldProducts")]
+    public class SoldProductsCount
+    {
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlArray("products")]
+        public SoldProduct[] Products { get; set; } = null!;
+    }
+
+    [XmlType("Product")]
+    public class SoldProduct
+    {
+        [XmlElement("name")]
+        public string Name { get; set; } = null!;
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+    }
+}
