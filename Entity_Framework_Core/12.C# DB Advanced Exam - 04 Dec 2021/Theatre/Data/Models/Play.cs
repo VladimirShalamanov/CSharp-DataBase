52871ba142d2108fcfcaa586f9df34589aa0a1df
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-08-23 00:26:02 +0300

    Add files via upload

---
+++ Entity_Framework_Core/12.C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Play.cs
@@ -0,0 +1,38 @@
+﻿namespace Theatre.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Enums;
+
+    public class Play
+    {
+        public Play()
+        {
+            this.Casts = new HashSet<Cast>();
+            this.Tickets = new HashSet<Ticket>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+
+        public TimeSpan Duration { get; set; }
+
+        public float Rating { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string Screenwriter { get; set; } = null!;
+
+        public ICollection<Cast> Casts { get; set; }
+
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
---
+++ Entity_Framework_Core/12.C# DB Advanced Exam - 04 Dec 2021/Theatre/Data/Models/Theatre.cs
@@ -0,0 +1,26 @@
+﻿namespace Theatre.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Net.Sockets;
+
+    public class Theatre
+    {
+        public Theatre()
+        {
+            this.Tickets = new HashSet<Ticket>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+        
+        public sbyte NumberOfHalls { get; set; }
+
+        [Required]
+        public string Director { get; set; } = null!;
+
+       public ICollection<Ticket> Tickets { get; set; }
+    }
+}
