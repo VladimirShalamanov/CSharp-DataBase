73082c80bc0fb4ba0617ff2330ca0cec11d10976
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2023-08-30 19:47:10 +0300

    Add files via upload

---
+++ Entity_Framework_Core/14.C# EXAM DB Advanced - 01 April 2023/Boardgames/Data/Models/Boardgame.cs
@@ -0,0 +1,37 @@
+﻿namespace Boardgames.Data.Models
+{
+    using Boardgames.Data.Models.Enums;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Boardgame
+    {
+        public Boardgame()
+        {
+            this.BoardgamesSellers = new HashSet<BoardgameSeller>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        public double Rating { get; set; }
+
+        public int YearPublished { get; set; }
+
+        [Required]
+        public CategoryType CategoryType { get; set; }
+
+        [Required]
+        public string Mechanics { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Creator))]
+        public int CreatorId { get; set; }
+        public Creator Creator { get; set; } = null!;
+
+        public ICollection<BoardgameSeller> BoardgamesSellers { get; set; } = null!;
+    }
+}
